2cd58af69ef91cd19ca6f2452f514a510fa3fbd4
Kamal Bisht <111355794+kml562@users.noreply.github.com>
2023-05-31 22:47:49 +0530

    done with

--- src/utils/validation/validatior.js
+++ src/utils/validation/validatior.js
@@ -9,11 +9,8 @@ export const isValidEmail = (email) => {
 };
 
 export const isValidPassword = (password) => {
-  const passwordRegex =
-  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{5,15}$/;
-    // /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{5,15}$/;
-  
-
+  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d@$!%*#?&]{5,15}$/;
+  //  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{5,15}$/;
   return passwordRegex.test(password);
 };
 
